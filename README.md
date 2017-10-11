b00b377f0e3fa7be5fd408d419daf89ce1265f14
ProgrammingLanguageLeader <vip.shoroch@gmail.com>
2017-10-11 03:05:31 +0300

    Updated lang_frequency.py and README.md

--- README.md
+++ README.md
@@ -1,6 +1,38 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+The script calculates a frequency of words in the text 
+and outputs the most popular words.
+
+# Hot to use
+
+This script requires python interpreter version 3.5 or higher.
+
+Type the following command to see a list of available arguments:
+```
+python lang_frequency.py -h
+```
+
+## Examples
+```
+python lang_frequency.py -n 4
+Specify a text file to analyze: file.txt
+ 1. в
+ 2. и
+ 3. на
+ 4. скрипт
+ 
+python lang_frequency.py -i file.txt
+ 1. в
+ 2. и
+ 3. на
+ 4. скрипт
+ 5. не
+ 6. можно
+ 7. такой
+ 8. вход
+ 9. должен
+ 10. с
+```
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,76 @@
+import argparse
+import string
 
 
 def load_data(filepath):
-    pass
+    try:
+        with open(filepath, 'r', encoding='utf-8') as file:
+            data = file.read()
+        return data
+    except IOError:
+        return None
 
 
-def get_most_frequent_words(text):
-    pass
+def delete_special_chars(text):
+    special_chars_string = string.punctuation
+    for char in special_chars_string:
+        text = text.replace(char, '')
+    return text
+
+
+def get_most_frequent_words(text, count):
+    words_from_text = {}
+    for word in text.split():
+        if words_from_text.get(word.lower()):
+            words_from_text[word.lower()] += 1
+        else:
+            words_from_text[word.lower()] = 1
+    the_most_frequent_words = sorted(
+        words_from_text,
+        key=words_from_text.get,
+        reverse=True
+    )[:count]
+    return the_most_frequent_words
+
+
+def print_the_most_frequent_words(words_list):
+    for index, word in enumerate(words_list):
+        print(' {}. {}'.format(index + 1, word))
 
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser(
+        description='The script calculates a frequency of words '
+                    'in the text and outputs the most popular words'
+    )
+    parser.add_argument(
+        '-i',
+        '--input',
+        default='',
+        help='A path to the file containing text to analyze',
+    )
+    parser.add_argument(
+        '-n',
+        '--number',
+        type=int,
+        default=10,
+        help='A number of popular words that will be printed by the program'
+    )
+
+    args = parser.parse_args()
+    input_path = args.input
+    if input_path == '':
+        input_path = input('Specify a text file to analyze: ')
+    words_number = args.number
+    if words_number <= 0:
+        print('-n parameter must be positive')
+        exit(0)
+
+    raw_text = load_data(input_path)
+    if not raw_text:
+        print('Check an input file')
+        exit(0)
+    text = delete_special_chars(raw_text)
+    the_most_frequent_words = get_most_frequent_words(text, words_number)
+    print_the_most_frequent_words(the_most_frequent_words)
+
